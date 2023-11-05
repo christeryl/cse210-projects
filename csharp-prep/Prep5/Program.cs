22e48b95063961960c02cd205458485afa9ec037
chris_teryl <joh19058@byui.edu>
2023-11-04 22:54:33 -0600

    Program with functions that squares a number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Welcome();
+
+        string userName = UserName();
+        int userNumber = UserNumber();
+
+        int squaredNumber = SquaredNumber(userNumber);
+
+        NameAndSquaredNumber(userName, squaredNumber);
+    }
+
+    static void Welcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string UserName()
+    {
+        Console.Write("Enter your name! ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int UserNumber()
+    {
+        Console.Write("Enter your favorite number! ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+
+    static int SquaredNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void NameAndSquaredNumber(string name, int square)
+    {
+      Console.WriteLine($"{name}, the square of your numbere is {square}!");  
     }
 }
