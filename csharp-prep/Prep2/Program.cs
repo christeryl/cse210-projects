3f1347e7dd4cbc93ed96c17ba33e9e9cbe7dd28c
chris_teryl <joh19058@byui.edu>
2023-11-04 19:28:30 -0600

    Gives user letter grade, and if they passed or not

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage ");
+        string valueFromUser = Console.ReadLine();
+
+        int g = int.Parse(valueFromUser);
+
+        string letter = "";
+
+        
+        //takes the users grade percentage and displays to the user what letter
+        //grade they got. 
+        if (g >= 90)
+        {
+            letter = "A";
+        }
+        else if (g <= 89 && g >= 80)
+        {
+            letter = "B";
+        }
+        else if (g <= 79 && g >= 70)
+        {
+            letter = "C";
+        }
+        else if (g <= 69 && g >= 60)
+        {
+            letter = "D";
+        }
+        else if (g <= 59)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade: {letter}");
+
+        //Displays to the user whether they passed or not.
+        if (g >= 70)
+        {
+            Console.WriteLine("Congratualtions! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass! Try again! You got this!");
+        }
+        
     }
 }
