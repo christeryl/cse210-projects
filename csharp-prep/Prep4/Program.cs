633663557ef1586552c31a26b9364f24b8b21f94
chris_teryl <joh19058@byui.edu>
2023-11-04 21:42:01 -0600

    User created list that finds sum, avg, & max

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List <int> numbers = new List <int>();
+
+        //Find the numbers added up
+        int usernum = -1;
+        while (usernum != 0)
+        {
+            Console.Write("Enter numbers. When you are done, press 0. ");
+
+            string userinput = Console.ReadLine();
+            usernum = int.Parse(userinput);
+
+            if (usernum != 0)
+            {
+                numbers.Add(usernum);
+            }
+        }
+        int sum = 0;
+        
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The list added up equals {sum}.");
+
+        //Find the average of the list
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average of the inputs is {average}.");
+
+        //Find the max value of the list
+
+        int maxnum = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maxnum)
+            {
+                maxnum = number;
+            }
+        }
+        Console.WriteLine($"The max number is {maxnum}");
     }
 }
